3f968504039064d93847e76173a58fadb971d5d5
Blackbaud-EricSlater <eric.slater@blackbaud.com>
2017-04-06 14:23:41 -0500

    finish first pass at action item list

--- src/action-items/action-items.component.ts
+++ src/action-items/action-items.component.ts
@@ -3,35 +3,70 @@ import { OnInit } from '@angular/core';
 import { ActionItem } from '../domain/action-item';
 
 @Component({
-  selector: 'action-items',
-  templateUrl: './action-items.component.html',
-  styleUrls: ['./action-items.component.css']
+    selector: 'action-items',
+    templateUrl: './action-items.component.html',
+    styleUrls: ['./action-items.component.css']
 })
 export class ActionItemsComponent {
-  actionItems:ActionItem[];
+    actionItems: ActionItem[];
 
-  constructor() {
-  }
+    constructor() {}
 
-  ngOnInit() {
-    this.actionItems = [{
-      name: "test 1",
-      priority: 1,
-      type: "job",
-      source: "jenkins",
-      created: new Date().getTime()
-    }, {
-      name: "test 2",
-      priority: 2,
-      type: "job",
-      source: "jenkins",
-      created: new Date().getTime()
-    }, {
-      name: "test 3",
-      priority: 3,
-      type: "pr",
-      source: "github",
-      created: new Date().getTime()
-    }];
-  }
+    ngOnInit() {
+        this.getActionItemsList();
+        setInterval(() => {
+          this.getActionItemsList();
+        }, 10000);
+    }
+
+    getActionItemsList(): void {
+        this.actionItems = [{
+            name: "group-member-monitor-build",
+            priority: 3,
+            type: "Jenkins Build - Building",
+            source: "jenkins",
+            created: new Date().getTime()
+        },{
+            name: "bluemoon-core-build",
+            priority: 2,
+            type: "Jenkins Build - Broken",
+            source: "jenkins",
+            created: new Date().getTime()
+        },{
+            name: "LUM-1234-fixed-stuff",
+            priority: 1,
+            type: "Github Pull Request",
+            source: "github",
+            created: new Date().getTime()
+        }];
+        this.actionItems = this.sortByPriorityAndOpenDuration(this.actionItems);
+    }
+
+    sortByPriorityAndOpenDuration(actionItems: ActionItem[]) : ActionItem[] {
+        let red = actionItems.filter(function(a) {
+            return a.priority == 1;
+        });
+
+        let yellow = actionItems.filter(function(a) {
+            return a.priority == 2;
+        });
+
+        let green = actionItems.filter(function(a) {
+            return a.priority == 3;
+        });
+
+        return red.sort(this.sortByOpenDuration)
+            .concat(yellow.sort(this.sortByOpenDuration))
+            .concat(green.sort(this.sortByOpenDuration));
+    }
+
+    sortByOpenDuration(a,b) {
+        if (a.created > b.created) {
+            return 1;
+        } else if (a.created < b.created) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
