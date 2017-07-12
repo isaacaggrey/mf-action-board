9c4db03b1e8dd34e4c775393bda192b2e99ae5aa
Blackbaud-MikeLueders <Blackbaud-MikeLueders@users.noreply.github.com>
2017-07-12 11:45:26 -0500

    increase PR threshold to 4 hrs (#13)

--- src/config/app-config-constants.ts
+++ src/config/app-config-constants.ts
@@ -1,7 +1,7 @@
 export const GITHUB_TEAM = 'blackbaud/micro-cervezas';
 export const GITHUB_USER = 'Blackbaud-ChristopherCotar';
 export const GITHUB_TOKEN = '2e49ae60d9dd3ff79d60f9d3ce212b1a3d4676bf';
-export const GITHUB_PR_SLA_MS = 3600000;
+export const GITHUB_PR_SLA_MS = 14400000;
 export const JENKINS_ACTION_ITEM_SLA_MS = 43200000;
 export const JENKINS_ENV = [
   {
